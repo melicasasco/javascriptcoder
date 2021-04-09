ae36cc3ad9f3c66a59eab0128da60dc701bedd45
melicasasco <melaniecasasco@gmail.com>
2021-04-09 15:15:09 -0300

    desafio ajax

--- artistas.js
+++ artistas.js
@@ -5,24 +5,21 @@ function cargarArtistas() {
   $.getJSON('artistas.json', (res, estado) => {
     if (estado === "success") {
       console.log(res);
-      res.map(p=>artistas.push(p));
+      res.map(a=>artistas.push(a));
     } else {
       console.log('Error al cargar artistas.')
     }
   });
 }
 
-function agregarartistasAmiDom() {
+function agregarArtistaADom() {
   for(artista of artistas) {
     $('#artistas').append(`
       <div id="artista_${artista.id}">
-        <div class="row py-3">
-          <div class="col-3 px-3">
-            <img width=180 height=300 src="${artista.obra}">
-          </div>
+        <div class="row">
           <div class="col-6">
             <h2>${artista.nombre}</h2>
-            <p>${artista.tecnica} - ${artista.medidas}</p>
+            <p>${artista.obra} - ${artista.tecnica}</p>
             <p>${artista.año}</p>
           </div>
         </div>
@@ -31,15 +28,10 @@ function agregarartistasAmiDom() {
   }
 }
 
-function filtrarYOrdenar(busqueda, obra) {
+function filtrarYOrdenar() {
   for(artista of artistas) {
-    const coincideartista = artista.nombre.toLowerCase().substr(0,busqueda.length) === busqueda.toLowerCase();
-    const coincideObra = artista.obra.toLowerCase() === obra.toLowerCase();
-    if (coincideartista && coincideObra) {
+    const coincideNombre = artista.nombre.toLowerCase().substr(0,busqueda.length) === busqueda.toLowerCase();
       $(`#artista_${artista.id}`).show();
-    } else {
-      $(`#artista_${artista.id}`).hide();
-    }
   }
 }
 
@@ -48,11 +40,11 @@ cargarArtistas();
 
 // 2) ready
 $(() => {
-  agregarartistasAmiDom();
+  agregarArtistaADom();
   $("#formBuscar").submit((e) => {
     e.preventDefault();
     const busqueda = $("#buscar").val();
-    const obra = $("#obra option:selected").text();
-    filtrarYOrdenar(busqueda,obra);
+    filtrarYOrdenar();
   });
 });
+
